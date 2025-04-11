dca086b3dae7af3a8effcd2b3ce11e9ad7bc98b1
Teodora Sumurduc <126513009+TeodoraSumurduc@users.noreply.github.com>
2025-04-12 00:12:09 +0300

    dataset and dataloader

--- train_model.py
+++ train_model.py
@@ -11,42 +11,42 @@ from torch.utils.data import DataLoader, Dataset
 
 #sentdex neural networks from scratch
 
+class MelanomaDataset(Dataset):
+    def __init__(self):
+        self.data = np.load("melanoma_training_data.npy", allow_pickle=True)
+        self.train_X = torch.tensor(np.array([item[0] for item in self.data]), dtype=torch.float32)
+        self.train_X = self.train_X / 255
+        self.train_Y = torch.tensor(np.array([item[1] for item in self.data]), dtype=torch.float32)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img = self.train_X[idx]
+        label = self.train_Y[idx]
+        return img, label
+
+
 #50 x 50 pixels
 img_size = 50
 
-training_data = np.load("melanoma_training_data.npy", allow_pickle=True)
-
-# for row in training_data:
-#     print(row[0])
-#     print(row[1])
-#     break
-
-# putting all the image arrays into this tensor
-train_X = torch.tensor(np.array([item[0] for item in training_data]), dtype=torch.float32)
-train_X = train_X / 255
-
-# for row in train_X:
-#     print(row)
-#     break
-
-#one-hot vector labesl tensor
-train_Y = torch.tensor(np.array([item[1] for item in training_data]), dtype=torch.float32)
+dataset = MelanomaDataset()
+dataloader = DataLoader(dataset=dataset, batch_size=100, shuffle=True, num_workers=2)
+# first_data = dataset[0]
+# print(first_data)
+epochs = 2
+total_samples = len(dataset)
+nr_iterations = math.ceil(total_samples / 100)
+print(total_samples, nr_iterations)
 
 net = Net()
 
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 
-#mean squared error loss function
 loss_functions = nn.MSELoss()
 
-#how many images to process at once
-batch_size = 100
-
-#TO DO HIGHLY RECOMMENDED: use DataLoader class to load data in batches
-epochs = 2
-
 for epoch in range(epochs):
-    for i in range(0, len(train_X), batch_size):
+    for i, (images, labels) in enumerate(dataloader):
         print(f"Epoch {epoch + 1}, fraction complete: {i / len(train_X)}")
         batch_X = train_X[i : i + batch_size].view(-1, 1, img_size, img_size)
         batch_Y = train_Y[i : i + batch_size]
@@ -61,4 +61,60 @@ for epoch in range(epochs):
 
         optimizer.step()
 
-torch.save(net.state_dict(), "saved_model.pth")
+# dataiter = iter(dataloader)
+# data = dataiter.next()
+# features, labels = data
+# print(features, labels)
+
+
+#
+# training_data = np.load("melanoma_training_data.npy", allow_pickle=True)
+# # print(len(training_data))
+# # for row in training_data:
+# #     print(row)
+# #     # print(row[0])
+# #     # print(row[1])
+# #     break
+#
+# # putting all the image arrays into this tensor
+# train_X = torch.tensor(np.array([item[0] for item in training_data]), dtype=torch.float32)
+# train_X = train_X / 255
+#
+# # for row in train_X:
+# #     print(row)
+# #     break
+#
+# #one-hot vector labesl tensor
+# train_Y = torch.tensor(np.array([item[1] for item in training_data]), dtype=torch.float32)
+#
+# net = Net()
+#
+# optimizer = optim.Adam(net.parameters(), lr=0.001)
+#
+# #mean squared error loss function
+# loss_functions = nn.MSELoss()
+#
+# #how many images to process at once
+# batch_size = 100
+#
+# #TO DO HIGHLY RECOMMENDED: use DataLoader class to load data in batches
+# epochs = 2
+#
+# for epoch in range(epochs):
+#     for i in range(0, len(train_X), batch_size):
+#         #print(f"Epoch {epoch + 1}, fraction complete: {i / len(train_X)}")
+#         batch_X = train_X[i : i + batch_size].view(-1, 1, img_size, img_size)
+#         batch_Y = train_Y[i : i + batch_size]
+#
+#         optimizer.zero_grad()
+#         #reset gradients of model parameters to zero before this pass
+#
+#         outputs = net(batch_X)
+#         loss = loss_functions(outputs, batch_Y)
+#
+#         loss.backward() #backpropagation
+#
+#         optimizer.step()
+#
+
+# torch.save(net.state_dict(), "saved_model.pth")
