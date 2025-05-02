e98e54bcb2bf5ba033fa376f5bef3498105fc939
Teodora Sumurduc <126513009+TeodoraSumurduc@users.noreply.github.com>
2025-05-02 18:58:57 +0300

    dockerfile

--- request.py
+++ request.py
@@ -1,15 +1,16 @@
 import requests
 
-url = "https://melanoma-detection-ai-563719261463.us-central1.run.app/predict"
-image_path = "demo_pics/melanoma_10117.jpg"  # pune aici calea unei imagini reale
-
-with open(image_path, 'rb') as img:
-    files = {'image': img}
-    response = requests.post(url, files=files)
-
-print("Status code:", response.status_code)
-print("Response:", response.text)
-
-# resp = requests.post("http://127.0.0.1:5000/predict", files={'image': open("demo_pics/melanoma_10179.jpg", 'rb')})
+# url = "https://melanoma-detection-ai-563719261463.us-central1.run.app/predict"
+# image_path = "demo_pics/melanoma_10117.jpg"  # pune aici calea unei imagini reale
 #
-# print(resp.json())
+# with open(image_path, 'rb') as img:
+#     files = {'image': img}
+#     response = requests.post(url, files=files)
+#
+# print("Status code:", response.status_code)
+# print("Response:", response.text)
+
+resp = requests.post("https://melanoma-detection-ai-563719261463.us-central1.run.app/predict", files={'image': open("demo_pics/melanoma_10179.jpg", 'rb')})
+
+print("Status code:", resp.status_code)
+print("Response:", resp.text)
