90be5b814b58546dafb2de8495278646526bfbb7
Teodora Sumurduc <126513009+TeodoraSumurduc@users.noreply.github.com>
2025-05-02 17:14:55 +0300

    changed dockerfile

---
+++ request.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://melanoma-detection-ai-563719261463.us-central1.run.app/predict"
+image_path = "demo_pics/melanoma_10117.jpg"  # pune aici calea unei imagini reale
+
+with open(image_path, 'rb') as img:
+    files = {'image': img}
+    response = requests.post(url, files=files)
+
+print("Status code:", response.status_code)
+print("Response:", response.text)
