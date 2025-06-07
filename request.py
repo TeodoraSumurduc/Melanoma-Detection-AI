82638ad056f2755f4b975847c3765e9d51f9574a
Teodora Sumurduc <126513009+TeodoraSumurduc@users.noreply.github.com>
2025-06-07 18:10:51 +0300

    commit

--- request.py
+++ request.py
@@ -10,7 +10,48 @@ import requests
 # print("Status code:", response.status_code)
 # print("Response:", response.text)
 
-resp = requests.post("https://melanoma-detection-ai.onrender.com/predict", files={'image': open("demo_pics/melanoma_10179.jpg", 'rb')})
+import os
+import shutil
+import random
 
-print("Status code:", resp.status_code)
-print("Response:", resp.text)
+# Calea către folderul original de test
+original_test_dir = 'demo_pics/test'
+# Calea unde vom salva noile foldere
+base_output_dir = 'melanoma_cancer_dataset'
+
+# Folderele noi
+val_dir = os.path.join(base_output_dir, 'val')
+new_test_dir = os.path.join(base_output_dir, 'new_test')  # redenumim pentru a evita confuzii
+
+# Clasele
+classes = ['benign', 'malignant']
+
+# Creează folderele de ieșire
+for target_dir in [val_dir, new_test_dir]:
+    for cls in classes:
+        os.makedirs(os.path.join(target_dir, cls), exist_ok=True)
+
+# Mutarea fișierelor
+for cls in classes:
+    cls_dir = os.path.join(original_test_dir, cls)
+    images = os.listdir(cls_dir)
+    random.shuffle(images)
+
+    split_point = len(images) // 2
+    val_images = images[:split_point]
+    test_images = images[split_point:]
+
+    # Mutăm în val
+    for img in val_images:
+        src = os.path.join(cls_dir, img)
+        dst = os.path.join(val_dir, cls, img)
+        shutil.move(src, dst)
+
+    # Mutăm în new_test
+    for img in test_images:
+        src = os.path.join(cls_dir, img)
+        dst = os.path.join(new_test_dir, cls, img)
+        shutil.move(src, dst)
+
+print("Împărțirea în seturi de validare și testare a fost realizată cu succes.")
+
--- train_model.py
+++ train_model.py
@@ -42,6 +42,8 @@ class TrainModel:
         self.malignant_training_folder = "melanoma_cancer_dataset/train/malignant/"
         self.benign_testing_folder = "melanoma_cancer_dataset/test/benign/"
         self.malignant_testing_folder = "melanoma_cancer_dataset/test/malignant/"
+        self.benign_val_folder = "melanoma_cancer_dataset/val/benign/"
+        self.malignant_val_folder = "melanoma_cancer_dataset/val/malignant/"
 
     def data_loader(self, dataset, collate_fn_melanoma=None):
         return DataLoader(dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers,
@@ -77,14 +79,20 @@ class TrainModel:
         malignant_testing_dataset = MelanomaDataset(self.malignant_testing_folder, 1,
                                                     transform=self.get_test_transform())
 
+        benign_val_dataset = MelanomaDataset(self.benign_val_folder, 0, transform=self.get_test_transform())
+        malignant_val_dataset = MelanomaDataset(self.malignant_val_folder, 1,
+                                                transform=self.get_test_transform())
+
         train_dataset = ConcatDataset([benign_training_dataset, malignant_training_dataset])
         test_dataset = ConcatDataset([benign_testing_dataset, malignant_testing_dataset])
+        val_dataset = ConcatDataset([benign_val_dataset, malignant_val_dataset])
 
-        return train_dataset, test_dataset
+        return train_dataset, test_dataset, val_dataset
 
-    def train_model(self, train_dataset, test_dataset):
+    def train_model(self, train_dataset, test_dataset, val_dataset):
         train_dataloader = self.data_loader(train_dataset)
         test_dataloader = self.data_loader(test_dataset)
+        val_dataloader = self.data_loader(val_dataset)
 
         loss_fn = nn.CrossEntropyLoss(weight=torch.tensor([0.5, 4.0]).to(self.device)) #favorizez melanomul
         # loss_fn = nn.CrossEntropyLoss()
@@ -116,7 +124,7 @@ class TrainModel:
             self.model.eval()
             val_loss = 0.0
             with torch.no_grad():
-                for batch in test_dataloader:
+                for batch in val_dataloader:
                     images, labels = batch
 
                     images = images.to(self.device)
@@ -126,7 +134,7 @@ class TrainModel:
                     loss = loss_fn(outputs, labels)
                     val_loss += loss.item()
 
-            val_loss /= len(test_dataloader)
+            val_loss /= len(val_dataloader)
 
             train_losses.append(train_loss)
             val_losses.append(val_loss)
@@ -206,9 +214,9 @@ class TrainModel:
 
 
 def main():
-    train_model = TrainModel()
-    train_dataset, test_dataset = train_model.get_dataset()
-    train_model.train_model(train_dataset, test_dataset)
+    cfg = TrainModel()
+    train_dataset, test_dataset, val_dataset = cfg.get_dataset()
+    cfg.train_model(train_dataset, test_dataset, val_dataset)
 
 
 if __name__ == "__main__":
