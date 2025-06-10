02a1c41112d12e85b5e59a6479e52cb2f92041e5
Teodora Sumurduc <126513009+TeodoraSumurduc@users.noreply.github.com>
2025-06-11 00:43:43 +0300

    commit

--- nn_class.py
+++ nn_class.py
@@ -9,29 +9,27 @@ img_size = 64
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        # Convolutional layers
         self.conv1 = nn.Conv2d(3, 64, 5, padding=2)
         self.bn1 = nn.BatchNorm2d(64)
         self.conv2 = nn.Conv2d(64, 128, 5, padding=2)
         self.bn2 = nn.BatchNorm2d(128)
         self.conv3 = nn.Conv2d(128, 256, 5, padding=2)
         self.bn3 = nn.BatchNorm2d(256)
-
-        # Fully connected layers
-        self.fc1 = nn.Linear(256 * 8 * 8, 512)  # După 3x max pooling => 64 → 32 → 16 → 8
-        self.fc2 = nn.Linear(512, 128)
-        self.fc3 = nn.Linear(128, 2)
+        self.conv4 = nn.Conv2d(256, 512, 5, padding=2)
+        self.bn4 = nn.BatchNorm2d(512)
+        self.fc1 = nn.Linear(512 * 4 * 4, 512)
+        self.fc2 = nn.Linear(512, 2)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
-        x = F.max_pool2d(x, 2)  # 64x64 -> 32x32
+        x = F.max_pool2d(x, 2)
         x = F.relu(self.bn2(self.conv2(x)))
-        x = F.max_pool2d(x, 2)  # 32x32 -> 16x16
+        x = F.max_pool2d(x, 2)
         x = F.relu(self.bn3(self.conv3(x)))
-        x = F.max_pool2d(x, 2)  # 16x16 -> 8x8
-
-        x = x.view(-1, 256 * 8 * 8)
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.max_pool2d(x, 2)
+        x = x.view(-1, 512 * 4 * 4)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
