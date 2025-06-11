6f2f846b4e8c3e48c2cd87f429a970b1510bdf84
Teodora Sumurduc <126513009+TeodoraSumurduc@users.noreply.github.com>
2025-06-11 14:12:36 +0300

    commit

--- train_model.py
+++ train_model.py
@@ -79,7 +79,7 @@ class config:
             val_loader = self.data_loader(val_subset, collate_fn_melanoma, shuffle=False)
 
             model = Net().to(self.device)
-            loss_fn = nn.CrossEntropyLoss(weight=torch.tensor([0.5, 4.0]).to(self.device)) #favorizez melanomul
+            loss_fn = nn.CrossEntropyLoss(weight=torch.tensor([0.25, 4.0]).to(self.device)) #favorizez melanomul
             optimizer = optim.Adam(model.parameters(), lr=self.learning_rate)
 
             train_losses = []
@@ -146,7 +146,7 @@ class config:
         # Antrenare finală pe tot setul
         train_loader = self.data_loader(dataset, collate_fn_melanoma)
         model = Net().to(self.device)
-        loss_fn = nn.CrossEntropyLoss(weight=torch.tensor([0.5, 4.0]).to(self.device)) #favorizez melanomul
+        loss_fn = nn.CrossEntropyLoss(weight=torch.tensor([0.25, 4.0]).to(self.device)) #favorizez melanomul
         optimizer = optim.Adam(model.parameters(), lr=self.learning_rate)
 
         train_losses = []
